7fcb2cbf92e9339d4d92da4c1432555572595ec5
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-03-18 01:02:13 +0000

    Working through naming conventions and debugging introduction.

---
+++ js-pre-course/debugging-intro/script.js
@@ -0,0 +1,22 @@
+// 1. Start with a headline and some article content
+const currentHeadline = "News in the world today"
+const currentArticle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy auctor."
+
+// "is or has" for boolean storing ariables and "amount or number" for integer values
+const isFakeNews = false;
+const amountOfWords = 957;
+
+// 2. Before we start we need to make sure the headline isn't too long
+if(headline.length > 20) {
+    headline = currentArticle + "...";
+}
+
+// 3. Show it to the user (in terminal anyway)
+console.log("Nology Post 2021")
+console.log(`Headline: ${headline}`)
+console.log(article);
+
+// 4. Example of a meaningful funciton name.. getData() is vague
+function getPost() {
+    //Get your post in here..
+}
