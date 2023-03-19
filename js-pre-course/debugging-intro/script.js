51ffa383339cb2962c877c89b49d40b62e30cd85
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-03-19 22:56:17 +0000

    Explored use of debugging tools and learning resources like stack overflow/w3schools.

--- js-pre-course/debugging-intro/script.js
+++ js-pre-course/debugging-intro/script.js
@@ -1,5 +1,5 @@
 // 1. Start with a headline and some article content
-const currentHeadline = "News in the world today"
+let currentHeadline = "News in the world today"
 const currentArticle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy auctor."
 
 // "is or has" for boolean storing ariables and "amount or number" for integer values
@@ -7,14 +7,14 @@ const isFakeNews = false;
 const amountOfWords = 957;
 
 // 2. Before we start we need to make sure the headline isn't too long
-if(headline.length > 20) {
-    headline = currentArticle + "...";
+if(currentHeadline.length > 15) {  //.length is a helper method, giving the length of the string
+    currentHeadline = currentHeadline.substring(0, 15) + "..."; //.substring() method to shorten string to assigned length
 }
 
 // 3. Show it to the user (in terminal anyway)
 console.log("Nology Post 2021")
-console.log(`Headline: ${headline}`)
-console.log(article);
+console.log(`Headline: ${currentHeadline}`)
+console.log(currentArticle);
 
 // 4. Example of a meaningful funciton name.. getData() is vague
 function getPost() {
