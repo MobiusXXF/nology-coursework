23edb82dd8bd6dd840cecb699a36dbd472044501
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-03-22 22:19:43 +0000

    Updated JS pre course folder.

---
+++ 03-js-pre-course/loops-arrays-iterators/arrays.js
@@ -0,0 +1,7 @@
+// coaches array
+const coaches = ["Anthony", "Mark", "Ruben"]
+console.log(coaches[0])
+
+// pet array
+const pets = ["Killer", "Bella", "Rocko"]
+console.log(pets[2])
---
+++ 03-js-pre-course/loops-arrays-iterators/loops.js
@@ -0,0 +1,19 @@
+// example array
+const myCoding = ["html", "css", "js", "python"]
+const evenNumbers = [2, 4, 6, 8, 10]
+
+// a sample for loop
+for (let index = 0; index < 5; index++) {
+    console.log(`The index of this loop was ${index}`)
+}
+
+// array combined with a for loop
+for (let index = 0; index < evenNumbers.length; index++) {
+    console.log(evenNumbers[index])
+}
+
+
+// array combined with a for loop
+for (let index = 0; index < myCoding.length; index++) {
+    console.log(myCoding[index])
+}
